584f58befd2c661303c6f20964a2a377bad92fdd
Supreet Kaur <supreetkaur4154@gmail.com>
2025-06-08 12:11:49 +0530

    Initial commit

---
+++ venv312/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!D:\locked\Projects\Training 4th sem\all projects\HospitalManagement_Django Adv\HospitalManagement_Django\venv312\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
